6e7b5c8665e46b8973da1c37b1f40e176a3644cc
Jared Brown <jaredbrown1@hotmail.com>
2021-06-28 22:30:51 -0700

    progress bar filling up according to song duration

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -85,6 +87,18 @@ function nextSong() {
 // On Load - Select First song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar & time
+function updateProgressBar(e) {
+	if (isPlaying) {
+		const { duration, currentTime } = e.srcElement;
+
+		// update progress bar width
+		const progressPercent = (currentTime / duration) * 100;
+		progress.style.width = `${progressPercent}%`;
+	}
+}
+
 // Event Listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);

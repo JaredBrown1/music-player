20c40091f0eb9006017af5ebfa634c49a6d2650c
Jared Brown <jaredbrown1@hotmail.com>
2021-06-28 21:43:10 -0700

    pause play button changes based on current state

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ let isPlaying = false;
 // Play
 function playSong() {
 	isPlaying = true;
+	playBtn.classList.replace("fa-play", "fa-pause");
+	playBtn.setAttribute("title", "Pause");
 	music.play();
 }
 
 // pause
 function pauseSong() {
 	isPlaying = false;
+	playBtn.classList.replace("fa-pause", "fa-play");
+	playBtn.setAttribute("title", "Play");
 	music.pause();
 }
 // Play or pause
